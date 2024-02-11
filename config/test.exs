d30724c75cb0357b49fecf2c96c4ed5a3a094341
Guilherme Bordallo <bordallog@Guilhermes-MacBook-Pro.local>
2024-02-11 01:12:31 -0500

    chore: configurating dialyzer, logs, credo, tests and factories

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :meme_game, MemeGame.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   hostname: "localhost",
   database: "meme_game_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
--- mix.exs
+++ mix.exs
@@ -57,7 +57,10 @@ defmodule MemeGame.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
